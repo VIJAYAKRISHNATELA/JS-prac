70e1879fb74fb9ebd8e3ff2f4f122c324a66dc37
vijay <vijayakrishnatela@gmail.com>
2026-05-02 17:08:51 +0530

    Revision of js day2

---
+++ dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3)//In Js we will get a dailouge box if we run this in brower's console since we are using node we wont be able to check that
+
+console.log(2+2)
+ console.log("krishns")
+
+ let name="vijay"
+ let age=20
+ let isLogggedIn=false
+
+ //null ==>standalone value
+ //undefines =>
+//symbol-->unique and immutable
+
+console.log(typeof undefined)
+console.log(typeof null)
+
---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId=83724
+let accountemail="javascript@gmmaill.com"
+var accountpassword="12345678"
+accountCity="Delhi";
+let accountState;
+
+// accountId=2//not allowed
+
+accountEmail="krishana@gmail.com"
+accountpassword="294378";
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+/* Never use var beause of issue in block scope and fucntional scope*/
+
+
+console.log([])
+console.table([accountEmail,accountpassword,accountCity,accountState])
