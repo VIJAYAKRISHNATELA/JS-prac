f0114ace33b8d8c267be5316060fbaffc24c38a5
vijay <vijayakrishnatela@gmail.com>
2026-05-02 18:35:05 +0530

    Revision of the Conversions logic and console logs

---
+++ conversionOperation.js
@@ -0,0 +1,26 @@
+// let score=false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// let isLoggenIn="k"
+// let booleanIsLoggedIn=Boolean(isLoggenIn)
+// console.log(booleanIsLoggedIn);
+
+
+//"22"-->22
+//"22abc"->NaN
+//true=>1; false->0
+//""->false
+//"vij"->true
+
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+console.log(typeof someNumber);
+
+
