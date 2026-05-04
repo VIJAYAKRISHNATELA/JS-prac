a32d9958d48771c860edf1bd37fd62f19e885b1a
vijay <vijayakrishnatela@gmail.com>
2026-05-04 22:57:36 +0530

    object destrcuting

--- objects2.js
+++ objects2.js
@@ -9,7 +9,7 @@ student.id=1
 student.name="vijay"
 student.isLoggedIn=false
 
-console.log(student)
+// console.log(student)
 
 
 //objects inside object
@@ -24,21 +24,81 @@ const cseStudnet={
     }
 }
 
-console.log(cseStudnet)
-console.log(cseStudnet.fullname.userfullname.firstname)
+// console.log(cseStudnet)
+// console.log(cseStudnet.fullname.userfullname.firstname)
 
 
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 
+
 // const obj3={obj1,obj2}
 
+// const obj3=Object.assign(obj1,obj2)
+
+// const obj3= Object.assign({}, obj1,obj2)//It combines evry vakue from the two object {}..(Object.assign())
 // console.log(obj3)
 
-const obj3=Object.assign({},obj1,obj2)
-console.log(obj3);
+// const obj3=Object.assign({},obj1,obj2,obj4)
+// console.log(obj3);
+
+// const obj3={...obj1, ...obj2}//It comibes all the values from the 2 objects it wrokks like the amov ,jus the syntax difference
+// console.log(obj3);
 
 
+//databse vaues usally comes like the array of objects
 
+// ex:
+
+const users=[
+    {
+
+    },
+    {
+        id: 1,
+        email: "vijay@gmail.com"
+    },
+]
+
+users[0].email
+console.log(student);
+
+console.log(Object.keys(student));//we can get the keys of the object using this method in a array format 
+
+
+console.log(Object.values(student))//from here we can get the values of the object using this method in array format
+console.log(Object.entries(student))//we can get the evry key value in an array format
+
+
+ console.log(student.hasOwnProperty('isLoggedIn'))//checks whether this projerty is there ot not
+
+
+//Destructuring
+// taking values out of an object (or array) and putting them into variables easily.
+const course ={
+    coursename: "Js in Hindi",
+    price:"999",
+    courseInstructor:"krishna"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor}=course// we can jsut assign the name of our wish after : 
+
+console.log(instructor);
+
+//Destructing of the object
+const navbar = ({company}) => {
+console.log(company)
+}
+
+navbar(company="company") 
+
+
+[
+    {},
+    {},
+    {}
+]
 
 
