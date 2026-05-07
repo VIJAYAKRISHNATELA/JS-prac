1bacac57a9fa72dc081c78f1e57e14fa342cfc3d
vijay <vijayakrishnatela@gmail.com>
2026-05-07 10:13:19 +0530

    Practicing JS

--- variables.js
+++ variables.js
@@ -1,19 +1,113 @@
-const accountId=83724
-let accountemail="javascript@gmmaill.com"
-var accountpassword="12345678"
-accountCity="Delhi";
-let accountState;
+// const accountId=83724
+// let accountemail="javascript@gmmaill.com"
+// var accountpassword="12345678"
+// accountCity="Delhi";
+// let accountState;
 
-// accountId=2//not allowed
+// // accountId=2//not allowed
 
-accountEmail="krishana@gmail.com"
-accountpassword="294378";
-accountCity="Bengaluru"
+// accountEmail="krishana@gmail.com"
+// accountpassword="294378";
+// accountCity="Bengaluru"
 
-console.log(accountId);
+// console.log(accountId);
 
-/* Never use var beause of issue in block scope and fucntional scope*/
+// /* Never use var beause of issue in block scope and fucntional scope*/
 
 
-console.table([accountEmail,accountpassword,accountCity,accountState])
+// console.table([accountEmail,accountpassword,accountCity,accountState])
 
+
+
+//---------------PRACTICE-----------------
+// ✅ 1️⃣ variables.js
+// Task 1 — Student Info
+
+// const name="DIAZ";
+// let age=22;
+// let college="LPU";
+// isStudent=true;
+
+// console.log(`${name} is ${age} years old and studying in ${college} and she is a student${isStudent}`);
+
+// Task 2 — Swap Two Numbers
+
+// let a=10;
+// let b=20;
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log(a);
+// console.log(b);
+
+
+//Task 3 — Simple Calculator
+
+// let c=50;
+// let d=100;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+
+// ✅ 2️⃣ dataTypes.js
+
+
+// const s="vijay";
+// const n=null;
+// let k;
+// const v=10;
+// ishuman=true;
+
+// console.log(typeof v)
+// console.log(typeof(s))
+
+// console.log(n)
+// console.log(typeof ishuman)
+// console.log(typeof k)
+
+
+
+// Task 2
+// Create an object:
+// const student={
+//     name:"rahul",
+//     age:22
+// }
+
+// console.log(student.name,student.age)
+
+
+// ✅ 3️⃣ conversionOperation.js
+// Task 1
+
+// let l=10;
+// l="10";
+// let s="89098"
+// s=Number(s)
+// isteacher=true
+// isteacher=Number(isteacher)
+
+// console.log (typeof s)
+// console.log(typeof l)
+// console.log(typeof isteacher)
+
+
+// Task 2
+
+// let score="500"
+// score=Number(score)
+// score+=50
+// console.log(score)
+
+// ✅ 4️⃣ strings.js/
+
+// Task 1 — Reverse String
+
+let str="krishna"
+str=str.reverse(str);
+console.log(str)
