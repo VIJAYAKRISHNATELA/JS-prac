e280d2abd1d4b8363d71292b1564053c1d0b974c
vijay <vijayakrishnatela@gmail.com>
2026-05-08 12:01:37 +0530

    ...spread operator and practiced problems on arrays

---
+++ loops.js
@@ -0,0 +1,61 @@
+
+// for(let i=0;i<=10;i++){
+//     const element=i
+//     if(element==5){
+//         console.log("5 is the best number");
+//     }
+//     console.log(element)
+// }
+
+
+
+// for(let i=0;i<=10;i++){
+//     console.log(`outer loop: ${i}`);
+//     for(let j=0;j<=10;j++){
+//         console.log(`Inner loop value ${j} and inner loop${i}`);
+//         console.log(i+'*'+j+'='+i*j)
+//     }
+// }
+
+
+// let myArray=["krishna","rahul","chetan"]
+// console.log(myArray.length)
+// for(let i=0;i<myArray.length;i++){
+//     const element=myArray[i];
+//     console.log(element)
+// }
+
+
+//break and continue
+
+// for(let  i=0;i<=20;i++){
+
+//     if(i==5){
+//         console.log("5 is detected");
+//         continue;
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+// let index=0;
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index=index+2;
+// }
+
+
+//Maps
+//maps are uique and it will be i the order which we have defined
+const map=new Map()
+map.set('IN',"INDIA")
+map.set('USA',"United Sattes of AMerics")
+map.set('Fr',"France")
+
+console.log(map)
+
+for(const {key,value} of map){
+    console.log(key,':',value)
+}
+
+
+
--- variables.js
+++ variables.js
@@ -108,6 +108,154 @@
 
 // Task 1 — Reverse String
 
-let str="krishna"
-str=str.reverse(str);
-console.log(str)
+// let k="krishna";
+
+// for(let i=k.length-1;i>=0;i--){
+//     console.log(k[i]);
+// }
+
+
+
+//Task 2 — Count Vowels
+
+// let s="Ramesh";
+// let count=0;
+// for(let i=0;i<s.length;i++){
+//     if(s[i]=='a' || s[i]=='e' || s[i]=='i' ||
+//         s[i]=='o' || s[i]=='u'
+//     ){
+//         count++;
+//     }
+// }
+// console.log(count)
+
+
+// Task 3 — Palindrome Check
+
+// let str="madam";
+// let rev="";
+// let pali=str;
+
+// for(let i=str.length-1;i>=0;i--){
+//     rev+=str[i];
+// }
+// if(rev==str){
+//     console.log("Yes it is a palinfrome");
+// }
+// else{
+//     console.log("not a palinrome")
+// }
+
+//Task 4 Capitalize first letter.
+
+// let str="Ankit";
+// let capitalize=str.charAt().toUpperCase()+str.slice(1)
+// console.log(capitalize)
+
+
+// ✅ 5️⃣ numsandmath.js
+
+// Task 1 — Random Number Generator
+// Generate numbers between 1–100.
+
+// n=100;
+// let randomnumber=0;
+// for(let i=0;i<=100;i++){
+//    randomnumber= Math.random()*100;
+// }
+// console.log(randomnumber)
+
+
+// Task 2 — Dice Game
+
+
+// let n=6;
+// let randomdicevalue;
+// for(let i=1;i<=n;i++){
+//     randomdicevalue=Math.random()*100;
+// }
+// console.log(randomdicevalue)
+
+// Task 3 — BMI Calculator
+
+// let BMI
+// let weight=70;
+// let heightincm=175;
+// let heightinm=heightincm/100
+
+// BMI=weight/(heightinm)*(heightinm)
+
+// console.log(BMI)
+
+
+
+// let dice =Math.floor(Math.random()*6)+1;
+// console.log(dice)
+
+
+// let weightinKg=50;
+// let heightincm=180;
+// let heightinm=heightincm/100;
+// let bmi=weightinKg/(heightinm*heightinm);
+// console.log(bmi)
+
+
+
+// Task 1 — Sum of Array
+
+// let myArr=[10,20,30,40]
+// let sum=0;
+
+// for(let i=0;i<myArr.length;i++){
+//     sum+=myArr[i];
+// }
+// console.log(sum)
+
+
+// ✅ Task 2 — Find Largest Number
+
+// let myArr=[4,7,1,9,2]
+// let largestnumber=myArr[0]
+
+// for(let i=0;i<myArr.length;i++){
+//     if(myArr[i]>largestnumber){
+//         largestnumber=myArr[i];
+//     }    
+// }
+
+// console.log(largestnumber)
+
+// ✅ Task 3 — Count Even Numbers
+// let myArr=[1,2,3,4,5,6];
+// let count=0;
+// for(let i=0;i<myArr.length;i++){
+//         if(myArr[i]%2==0){
+//             count++;
+//         }
+// }
+// console.log(count)
+
+// ✅ Task 4 — Reverse Array
+
+// let myArr=[1,2,3,4];
+// for(let i=myArr.length-1;i>=0;i--){
+//     console.log(myArr[i])
+
+// }
+
+
+
+const states=["Andhra Pradesh","Telangana","TamilNadu"];
+const city=["vijayawada","Hyderabad","chennai"];
+
+
+// states.push(city)
+
+// console.log(states)
+
+// const allinfo=states.concat(city)
+// console.log(allinfo)
+
+
+const allinfo=[...states,...city]
+console.log(allinfo)
